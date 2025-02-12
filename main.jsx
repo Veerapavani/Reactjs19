6593f8741c06737099013ca524db170de03217dc
Veerapavani <2211cs010433@mallareddyuniversity.ac.in>
2025-02-12 11:39:50 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,40 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import UseEffect from './useeffect/UseEffect'
+//import "../src/styles/globally.css";
+// import './index.css'
+// import Cbc from './Component/Cbc.jsx'
+// import Fbc from './Component/Fbc.jsx'
+// import Demo from './Component/Demo.jsx'
+// import Parent from './props/Parent.jsx'
+// import State from './state/State.jsx'
+// import Inline from './styles/Inline.jsx'
+// import Comp1 from './styles/Comp1.jsx';
+// import Comp2 from './styles/Comp2.jsx';
+// import External from './styles/External.jsx';
+// /import Ref from './useRef/Ref.jsx'
+// import List from './List-keys/List.jsx'
+// import CompA from './useContext/CompA'
+// import CompB from './useContext/CompB'
+// import CompC from './useContext/CompC'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <Cbc />
+    <Fbc />
+    <Demo /> */}
+    {/* <Parent /> */}
+    {/* <State/> */}
+    {/* <Inline /> */}
+    {/* <Comp1/> */}
+    {/* <Comp2/> */}
+    {/* <External/> */}
+    {/* <List/> */}
+    {/* <Ref/> */}
+    {/* <CompA/> */}
+    {/* <CompB/> */}
+    {/* <CompC/> */}
+    <UseEffect/>
+
+  </StrictMode>,
+)
+
